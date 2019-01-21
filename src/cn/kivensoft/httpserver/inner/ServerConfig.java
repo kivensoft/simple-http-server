2553e1a3677fe4900e49700ea4ad7cb0804fbb41
kiven <kivensoft@gmail.com>
2019-01-21 14:49:07 +0800

    修正内置http服务器的配置文件引用未公开函数的警告

--- src/cn/kivensoft/httpserver/inner/ServerConfig.java
+++ src/cn/kivensoft/httpserver/inner/ServerConfig.java
@@ -8,104 +8,118 @@
 package cn.kivensoft.httpserver.inner;
 
 /**
- * Parameters that users will not likely need to set
- * but are useful for debugging
+ * Parameters that users will not likely need to set but are useful for
+ * debugging
  */
 
 class ServerConfig {
 
-    static int clockTick;
+	static int clockTick;
 
-    static int defaultClockTick = 10000 ; // 10 sec.
+	static int defaultClockTick = 10000; // 10 sec.
 
-    /* These values must be a reasonable multiple of clockTick */
-    static long defaultReadTimeout = 20 ; // 20 sec.
-    static long defaultWriteTimeout = 60 ; // 60 sec.
-    static long defaultIdleInterval = 300 ; // 5 min
-    static long defaultSelCacheTimeout = 120 ;  // seconds
-    static int defaultMaxIdleConnections = 200 ; 
+	/* These values must be a reasonable multiple of clockTick */
+	static long defaultReadTimeout = 20; // 20 sec.
+	static long defaultWriteTimeout = 60; // 60 sec.
+	static long defaultIdleInterval = 300; // 5 min
+	static long defaultSelCacheTimeout = 120; // seconds
+	static int defaultMaxIdleConnections = 200;
 
-    static long defaultDrainAmount = 64 * 1024;
+	static long defaultDrainAmount = 64 * 1024;
 
-    static long readTimeout;
-    static long writeTimeout;
-    static long idleInterval;
-    static long selCacheTimeout;	
-    static long drainAmount;	// max # of bytes to drain from an inputstream
-    static int maxIdleConnections;	
-    static boolean debug = false;
+	static long readTimeout;
+	static long writeTimeout;
+	static long idleInterval;
+	static long selCacheTimeout;
+	static long drainAmount; // max # of bytes to drain from an inputstream
+	static int maxIdleConnections;
+	static boolean debug = false;
 
-    static {
-	
-	idleInterval = ((Long)java.security.AccessController.doPrivileged(
-		new sun.security.action.GetLongAction(
-		"sun.net.httpserver.idleInterval", 
-		defaultIdleInterval))).longValue() * 1000;
-	
-	clockTick = ((Integer)java.security.AccessController.doPrivileged(
-		new sun.security.action.GetIntegerAction(
-		"sun.net.httpserver.clockTick", 
-		defaultClockTick))).intValue();
-	
-	maxIdleConnections = ((Integer)java.security.AccessController.doPrivileged(
-		new sun.security.action.GetIntegerAction(
-		"sun.net.httpserver.maxIdleConnections", 
-		defaultMaxIdleConnections))).intValue();
-	
-	readTimeout = ((Long)java.security.AccessController.doPrivileged(
-		new sun.security.action.GetLongAction(
-		"sun.net.httpserver.readTimeout", 
-		defaultReadTimeout))).longValue()* 1000;
-	
-	selCacheTimeout = ((Long)java.security.AccessController.doPrivileged(
-		new sun.security.action.GetLongAction(
-		"sun.net.httpserver.selCacheTimeout", 
-		defaultSelCacheTimeout))).longValue()* 1000;
-	
-	writeTimeout = ((Long)java.security.AccessController.doPrivileged(
-		new sun.security.action.GetLongAction(
-		"sun.net.httpserver.writeTimeout", 
-		defaultWriteTimeout))).longValue()* 1000;
-	
-	drainAmount = ((Long)java.security.AccessController.doPrivileged(
-		new sun.security.action.GetLongAction(
-		"sun.net.httpserver.drainAmount", 
-		defaultDrainAmount))).longValue();
-	
-	debug = ((Boolean)java.security.AccessController.doPrivileged(
-		new sun.security.action.GetBooleanAction(
-		"sun.net.httpserver.debug"))).booleanValue();
-    }
+	static {
 
-    static long getReadTimeout () {
-	return readTimeout;
-    }
+//		idleInterval = ((Long) java.security.AccessController
+//				.doPrivileged(new sun.security.action.GetLongAction(
+//						"sun.net.httpserver.idleInterval",
+//						defaultIdleInterval))).longValue()
+//				* 1000;
+		idleInterval = defaultIdleInterval * 1000;
 
-    static long getSelCacheTimeout () {
-	return selCacheTimeout;
-    }
+//		clockTick = ((Integer) java.security.AccessController
+//				.doPrivileged(new sun.security.action.GetIntegerAction(
+//						"sun.net.httpserver.clockTick", defaultClockTick)))
+//								.intValue();
+		clockTick = defaultClockTick;
 
-    static boolean debugEnabled () {
-	return debug;
-    }
+//		maxIdleConnections = ((Integer) java.security.AccessController
+//				.doPrivileged(new sun.security.action.GetIntegerAction(
+//						"sun.net.httpserver.maxIdleConnections",
+//						defaultMaxIdleConnections))).intValue();
+//
+		maxIdleConnections = defaultMaxIdleConnections;
+		
+//		readTimeout = ((Long) java.security.AccessController
+//				.doPrivileged(new sun.security.action.GetLongAction(
+//						"sun.net.httpserver.readTimeout", defaultReadTimeout)))
+//								.longValue()
+//				* 1000;
+		readTimeout = defaultReadTimeout * 1000;
+		
+//		selCacheTimeout = ((Long) java.security.AccessController
+//				.doPrivileged(new sun.security.action.GetLongAction(
+//						"sun.net.httpserver.selCacheTimeout",
+//						defaultSelCacheTimeout))).longValue()
+//				* 1000;
+		selCacheTimeout = defaultSelCacheTimeout * 1000;
 
-    static long getIdleInterval () {
-	return idleInterval;
-    }
+//		writeTimeout = ((Long) java.security.AccessController
+//				.doPrivileged(new sun.security.action.GetLongAction(
+//						"sun.net.httpserver.writeTimeout",
+//						defaultWriteTimeout))).longValue()
+//				* 1000;
+		writeTimeout = defaultWriteTimeout * 1000;
+		
 
-    static int getClockTick () {
-	return clockTick;
-    }
+//		drainAmount = ((Long) java.security.AccessController
+//				.doPrivileged(new sun.security.action.GetLongAction(
+//						"sun.net.httpserver.drainAmount", defaultDrainAmount)))
+//								.longValue();
+		drainAmount = defaultDrainAmount;
 
-    static int getMaxIdleConnections () {
-	return maxIdleConnections;
-    }
+//		debug = ((Boolean) java.security.AccessController
+//				.doPrivileged(new sun.security.action.GetBooleanAction(
+//						"sun.net.httpserver.debug"))).booleanValue();
+		debug = false;
+	}
 
-    static long getWriteTimeout () {
-	return writeTimeout;
-    }
+	static long getReadTimeout() {
+		return readTimeout;
+	}
 
-    static long getDrainAmount () {
-	return drainAmount;
-    }
+	static long getSelCacheTimeout() {
+		return selCacheTimeout;
+	}
+
+	static boolean debugEnabled() {
+		return debug;
+	}
+
+	static long getIdleInterval() {
+		return idleInterval;
+	}
+
+	static int getClockTick() {
+		return clockTick;
+	}
+
+	static int getMaxIdleConnections() {
+		return maxIdleConnections;
+	}
+
+	static long getWriteTimeout() {
+		return writeTimeout;
+	}
+
+	static long getDrainAmount() {
+		return drainAmount;
+	}
 }
